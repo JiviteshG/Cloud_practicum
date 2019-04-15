745d44e19f6d9bb58a77e502f6ccbbc12f8344c4
devanshuShrivastava <devanshu.srivastava1@gmail.com>
2019-04-15 05:46:57 -0300

    sddsfsf

--- CloudFlask/views.py
+++ CloudFlask/views.py
@@ -44,20 +44,18 @@ def login():
     db.session.add(user)
     db.commit()
     if request.method == 'POST':
-		userid = request.form['name']
-		get_user = Mortgage_details.query.filter_by(name=userid).first()
-		password = request.form['password']
+        userid = request.form['name']
+        get_user = Mortgage_details.query.filter_by(name=userid).first()
+        password = request.form['password']
         if get_user.password == password :
-			if get_user.address is None or get_user.phone_number is None or get_user.employer_info is None or get_user.salary is None or get_user.mortgage_value is None or get_user.start_date is None or get_user.m1sid is None or get_user.ins_value is None or get_user.ded_value: 
-				get_user.application_status='Complete'
-				print(get_user.application_status)
-			else: 
-				get_user.application_status='Incomplete'
-				print(get_user.application_status)
-			
-			return render_template('updatemessage2.html',mo=get_user)
-		else:
-			error = 'Employee ID and password do not match! Try again.'
-			return render_template('login.html', error = error)
-
-	return render_template('login.html')
+            if get_user.address is None or get_user.phone_number is None or get_user.employer_info is None or get_user.salary is None or get_user.mortgage_value is None or get_user.start_date is None or get_user.m1sid is None or get_user.ins_value is None or get_user.ded_value:
+                get_user.application_status='Complete'
+                print(get_user.application_status)
+            else:
+                get_user.application_status='Incomplete'
+                print(get_user.application_status)
+            return render_template('updatemessage2.html',mo=get_user)
+        else:
+            error = 'Employee ID and password do not match! Try again.'
+            return render_template('login.html', error = error)
+    return render_template('login.html')
