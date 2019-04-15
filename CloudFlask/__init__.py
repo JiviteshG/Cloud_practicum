b1bfb7a2f58745169aed034a96647731c4eeab3e
devanshuShrivastava <devanshu.srivastava1@gmail.com>
2019-04-15 05:03:41 -0300

    f

--- CloudFlask/__init__.py
+++ CloudFlask/__init__.py
@@ -4,7 +4,7 @@ The flask application package.
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.sqlalchemy import Heroku
+from flask.ext.heroku import Heroku
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/projectdb'
 heroku = Heroku(app)
