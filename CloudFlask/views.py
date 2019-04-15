60fcdd77727be3d6838300d86d795f6e852a37a3
devanshuShrivastava <devanshu.srivastava1@gmail.com>
2019-04-15 04:36:18 -0300

    s

--- CloudFlask/views.py
+++ CloudFlask/views.py
@@ -11,137 +11,28 @@ import requests
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 from flask_dance.contrib.github import make_github_blueprint, github 
 @app.route('/')
-def home(): 
-# if github.authorized:
-	print("A")
-	account_info = github.get('/user')
-	print(account_info)
-	git_username = ''
-	print(git_username)
-	print('Checking uname')
-	if account_info.ok:
-		print('Checking uname started')
-		account_info_json = account_info.json()
-		git_username = account_info_json['login']
-		print(git_username)
-		session['user'] = git_username
-		print("Reach")
-		return redirect(url_for('addEmployer',  username=session['user']))
-	return render_template('employer_home.html')
+def home():
+	return render_template('home.html')
 
-@app.route('/github_login', methods=['GET', 'POST'])
-def github_login():
-
-	
-
-	session.pop('user', None)
-	# print('1')
-	# if not twitter.authorized:
-	# 	print('2')
-	# 	return redirect(url_for('twitter.login'))
-	# print('3')
-	# account_info = twitter.get('account/settings')
-	# print('4')
-	# if account_info.ok:
-	# 	account_info_json = account_info.json()
-
-	# 	return '<h1> Your Twitter name is @{}'.format(account_info_json['screen_name'])
-
-	# Correct code
-	# if not github.authorized:
-	# 	return redirect(url_for('github.login'))
-	# account_info = github.get('/user')
-	# git_username = ''
-	# if account_info.ok:
-	# 	account_info_json = account_info.json()
-	# 	git_username = account_info_json['login']
-
-
-
-	f = open("log1.txt", "a+")
-	f.write("method: GET \nEnd-point: http://127.0.0.1:8001/login \nparameters: None\n" )
-	f.close()
-	# Employee_details1 = Employee_details.query.filter_by(username=username),first()
-	
-	
+@app.route('/login', methods=['GET', 'POST'])
+def login():
 
 	if request.method == 'POST':
-		f = open("log1.txt", "a+")
-		f.write("method: POST \nEnd-point: http://127.0.0.1:8001/login \nparameters: Login from git ")
-		f.close()
-
-		### CODE FOR OAUTH
-		print("Reach0")
-		if not github.authorized:
-			print("NA")
-			return redirect(url_for('github.login'))
-		print("A")
-		account_info = github.get('/user')
-		print(account_info)
-		git_username = ''
-		print(git_username)
-		print('Checking uname')
-		if account_info.ok:
-			print('Checking uname started')
-			account_info_json = account_info.json()
-			git_username = account_info_json['login']
-			print(git_username)
-			session['user'] = git_username
-			print("Reach")
-			return redirect(url_for('addEmployer',  username=session['user']))
-
-		## MAINSTREAM LOGIN
-		# for i in range(1):
-		# 	f.write("POST, http://127.0.0.1:8001/login, username: "+request.form['username']+", password: "+request.form['password']+" %d\r\n" % (i))
-		# emppass = request.form
-		# username = request.form['username']
-		# get_emp = Employee_details.query.filter_by(username=username).first()
-		# password = request.form['password']
-
-
-		# pathToXML = "Salt.xml"
-		# tree = ET.parse(pathToXML)
-		# root = tree.getroot()
-
-		# # all items data
-		# print('Expertise Data:'+str(root)+str(tree))
-		# salt = ''
-		# for elem in root:
-		# 	salt = elem.text
-
-
-		# if get_emp is None:
-		# 	error = 'User not present! Try again.'
-		# 	return render_template('login.html', error = error)
-		# if  get_emp.password != password+salt:
-		# 	error = 'Password do not match! Try again.'
-		# 	return render_template('login.html', error = error)
-		# else:
-		# session['user'] = username
-		# return redirect(url_for('addEmployer',  username=session['user']))
+		userid = request.form['name']
+		get_user = Mortgage_details.query.filter_by(name=userid).first()
+		password = request.form['password']
 		
-		
-	# return '<h1>Request failed! <h1>'
-	# return render_template('login.html')
-	return render_template('github_login.html')
+		if get_user.password == password :
+			if get_user.address is None or get_user.phone_number is None or get_user.employer_info is None or get_user.salary is None or get_user.mortgage_value is None or get_user.start_date is None or get_user.m1sid is None or get_user.ins_value is None or get_user.ded_value: 
+				get_user.application_status='Complete'
+				print(get_user.application_status)
+			else: 
+				get_user.application_status='Incomplete'
+				print(get_user.application_status)
+			
+			return render_template('updatemessage2.html',mo=get_user)
+		else:
+			error = 'Employee ID and password do not match! Try again.'
+			return render_template('login.html', error = error)
 
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+	return render_template('login.html')
